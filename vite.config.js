9d8981ce6f5a35881e3d6f83deee0d8ea3a1fa76
SUD77 <jaiswalsudhanshu20@gmail.com>
2024-03-17 11:49:37 +0530

    revert

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/tic-tac-toe-starting-project/",
+  base:"/gaurav-portfolio/",
   build:{
     outDir: "./docs"
   },
